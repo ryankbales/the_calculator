9756ca68fc44e600e3fd45a13c244c7c9ae29576
Ryan Bales <ryankbales@gmail.com>
2014-09-23 08:10:23 -0700

    working on it.

--- calculator.rb
+++ calculator.rb
@@ -1,32 +1,56 @@
 require 'pry'
 
-calc_image = "
- _____________________
-|  _________________  |
-| |              0. | |
-| |_________________| |
-|  ___ ___ ___   ___  |
-| | 7 | 8 | 9 | | + | |
-| |___|___|___| |___| |
-| | 4 | 5 | 6 | | - | |
-| |___|___|___| |___| |
-| | 1 | 2 | 3 | | x | |
-| |___|___|___| |___| |
-| | . | 0 | = | | / | |
-| |___|___|___| |___| |
-|_____________________|
 
-"
-puts calc_image
 
-puts "Enter numbers you wish to calculate separated by a space:"
+# user enters a number
+# user enters operator
+# user enters number
+# user either enters an operator and running total prints out, (t) to get total, keep "#{operating}" (c) to clear, (q) to quit
 
-num_input = gets.chomp.split.each_slice(1).map { |n| n.join ' '}
-numbers = []
-num_input.each do |n|
-  numbers << n.to_i
-end
 
-puts "(A)dd, (S)ubtract, (M)ultiply, (D)ivide?"
+begin
+  puts "Power On/Off?"
+  power = gets.chomp.downcase
+  
+  power_status = if power == "on"
+    true
+  else
+    false
+  end
 
+  calc_image = "
+   _____________________
+  |  _________________  |
+  | |              0. | |
+  | |_________________| |
+  |  ___ ___ ___   ___  |
+  | | 7 | 8 | 9 | | + | |
+  | |___|___|___| |___| |
+  | | 4 | 5 | 6 | | - | |
+  | |___|___|___| |___| |
+  | | 1 | 2 | 3 | | x | |
+  | |___|___|___| |___| |
+  | | . | 0 | = | | / | |
+  | |___|___|___| |___| |
+  |_____________________|
+
+  "
+
+  puts calc_image
+
+  puts "Enter numbers and hit 'c' at anytime to get clear the result."
+  input = []
+  operator = "+, -, *, /"
+  input << num1 = 0
+  input << num2 = 0
+  input << running_total = 0
+
+  puts "=>Num:"
+  input[0] = gets.chomp.to_i
+
+
+  
+
+
+end while power_status
 
